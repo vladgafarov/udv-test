e423a7552428c0523c57ce26ee713af48de5fca8
Vlad Gafarov <vlad.gafarov02@gmail.com>
2023-04-28 22:05:59 +0500

    add user in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,17 @@
-import { Title } from '@mantine/core'
+import { UserCircleIcon } from '@heroicons/react/24/outline'
+import { ActionIcon, Flex, Text, Title } from '@mantine/core'
 
 export default function Header() {
-	return <Title order={3}>Chat app</Title>
+	return (
+		<Flex justify={'space-between'} align={'center'} h="100%">
+			<Title order={3}>Chat app</Title>
+
+			<Flex align="center" gap="sm">
+				<Text>User123</Text>
+				<ActionIcon>
+					<UserCircleIcon />
+				</ActionIcon>
+			</Flex>
+		</Flex>
+	)
 }
