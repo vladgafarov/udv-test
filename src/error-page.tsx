72f671651af712a4118d7d72b8829a71f5983a67
Vlad Gafarov <vlad.gafarov02@gmail.com>
2023-05-13 20:04:32 +0500

    add pwa

--- src/error-page.tsx
+++ src/error-page.tsx
@@ -1,8 +1,7 @@
 import { useRouteError } from 'react-router-dom'
 
 export default function ErrorPage() {
-	const error = useRouteError()
-	console.error(error)
+	const error = useRouteError() as any
 
 	return (
 		<div id="error-page">
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,41 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			devOptions: {
+				enabled: true,
+			},
+			workbox: {
+				globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+			},
+			manifest: {
+				name: 'Chat app',
+				short_name: 'Chat',
+				description: 'Chat app for messaging within browser',
+				theme_color: '#5C7CFA',
+				background_color: '#5C7CFA',
+				icons: [
+					{
+						src: 'vite-pwa256.png',
+						sizes: '256x256',
+						type: 'image/png',
+					},
+					{
+						src: 'vite-pwa512.png',
+						sizes: '512x512',
+						type: 'image/png',
+					},
+				],
+			},
+		}),
+	],
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src'),
